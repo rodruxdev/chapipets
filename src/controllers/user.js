b195b1c902a56f143a2649ea23d5c0080d96544d
Rodrigo Goitia <rgoitia@ultragrupo.com>
2024-02-06 20:08:26 -0300

    ✨ Add logic in controllers in pet and users

---
+++ src/controllers/user.js
@@ -0,0 +1,54 @@
+import { UsersModel } from "../models/mysql/user.js";
+import { validatePartialUser, validateUser } from "../schemas/users.js";
+
+export class UsersController {
+  static async getAll(req, res) {
+    const result = validatePartialUser(req.body);
+    if (result.error) {
+      return res.status(400).json({ error: JSON.parse(result.error.message) });
+    }
+    const filters = result.data;
+    const filteredUsers = await UsersModel.getAll(filters);
+    res.json(filteredUsers);
+  }
+
+  static async getById(req, res) {
+    const { id } = req.params;
+    const user = await UsersModel.getById({ id });
+    if (user === undefined) {
+      return res.status(404).json({ message: "User don't found" });
+    }
+    res.json(user);
+  }
+
+  static async create(req, res) {
+    const result = validateUser(req.body);
+    if (result.error) {
+      return res.status(400).json({ error: JSON.parse(result.error.message) });
+    }
+    const newUser = await UsersModel.create({ input: result.data });
+    res.status(201).json(newUser);
+  }
+
+  static async update(req, res) {
+    const result = validatePartialUser(req.body);
+    if (result.error) {
+      return res.status(400).json({ error: JSON.parse(result.error.message) });
+    }
+    const id = Number(req.params.id);
+    const updatedUser = await UsersModel.update({ id, input: result.data });
+    if (updatedUser === undefined) {
+      return res.status(404).json({ message: "User don't found" });
+    }
+    res.status(200).json(updatedUser);
+  }
+
+  static async delete(req, res) {
+    const id = Number(req.params.id);
+    const deleteUserResult = await UsersModel.delete({ id });
+    if (!deleteUserResult) {
+      return res.status(404).json({ message: "User don't found" });
+    }
+    res.status(204);
+  }
+}
